60c2cb4ce33dc401e83e61e0be2b9a7662afe70e
ThonysArc <Gjkakahjk135@gmail.com>
2025-11-24 23:06:05 -0500

    docs: Agregar comentarios al código JavaScript

--- javascript2.js
+++ javascript2.js
@@ -1,9 +1,16 @@
 // Javascript 2 - Manipulación del DOM
+
+/**
+ * Cambia el color del párrafo a verde lima
+ */
 function cambiarColor() {
     const parrafo = document.querySelector('p');
     parrafo.style.color = 'lime';
 }
 
+/**
+ * Agrega texto dinámicamente al párrafo
+ */
 function agregarTexto() {
     const parrafo = document.querySelector('p');
     parrafo.innerHTML += "<br>Texto agregado dinámicamente";
